9f4e9b6f15e46f599d8f3892a11f6c66dcd50e49
Daniel Jimenez <srdejo@gmail.com>
2025-08-22 11:03:40 -0500

    Initial commit

---
+++ technical-test/src/test/java/io/github/srdejo/technical/test/TechnicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.srdejo.technical.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
